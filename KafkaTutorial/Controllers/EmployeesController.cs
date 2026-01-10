8c19992348856dadbdfb85cf9ccc91467f5d5569
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-10 14:42:18 -0300

    Create EmployeeController
    It has a get method GetEmployees, that gets all employees from the database, and a post method CreateEmployee, that creates a new employee in the database.

---
+++ KafkaTutorial/Controllers/EmployeesController.cs
@@ -0,0 +1,36 @@
+﻿using KafkaTutorial.Data;
+using KafkaTutorial.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KafkaTutorial.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class EmployeesController(AppDbContext dbContext) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+
+            IEnumerable<Employee> employees = await dbContext.Employees.ToListAsync();
+
+            return Ok(employees);
+        }
+
+        [HttpPost("{name}/{surname}")]
+        public async Task<ActionResult> CreateEmployee(string name, string surname)
+        {
+            Employee employee = new Employee
+            {
+                Name = name,
+                Surname = surname
+            };
+
+            dbContext.Employees.Add(employee);
+            await dbContext.SaveChangesAsync();
+
+            return Created();
+        }
+    }
+}
