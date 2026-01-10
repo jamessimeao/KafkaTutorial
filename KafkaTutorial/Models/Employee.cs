f8f05b96f357962b61d11048611dafe746e9aca5
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-10 13:59:49 -0300

    Create Employee model

---
+++ KafkaTutorial/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KafkaTutorial.Models
+{
+    public record Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+
+        public Employee(int id, string name, string surname)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+        }
+    }
+}
