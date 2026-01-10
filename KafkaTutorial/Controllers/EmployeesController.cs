33775556ba9ed5bd07ad0a2c50a84df3d7c8d409
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-10 15:35:50 -0300

    Configure CreateEmployee action to send a message to Kafka with the created employee

--- KafkaTutorial/Controllers/EmployeesController.cs
+++ KafkaTutorial/Controllers/EmployeesController.cs
@@ -1,7 +1,9 @@
-﻿using KafkaTutorial.Data;
+﻿using Confluent.Kafka;
+using KafkaTutorial.Data;
 using KafkaTutorial.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 namespace KafkaTutorial.Controllers
 {
@@ -45,6 +47,31 @@ namespace KafkaTutorial.Controllers
 
             // At least for in memory database, after dbContext.Employees.Add(employee)
             // the employee has the Id that was stored in the database, so we can return it.
+
+            /***************** Kafka *****************************/
+
+            // Message to publish
+            var message = new Message<int, string>()
+            {
+                Key = employee.Id,
+                Value = JsonSerializer.Serialize(employee),
+            };
+
+            // Client
+            ProducerConfig producerConfig = new ProducerConfig()
+            {
+                BootstrapServers = "localhost:9092",
+                Acks = Acks.All
+            };
+
+            ProducerBuilder<int, string> producerBuilder = new ProducerBuilder<int, string>(producerConfig);
+
+            using (IProducer<int, string> producer = producerBuilder.Build())
+            {
+                await producer.ProduceAsync("employeeTopic", message);
+            }
+            
+            /*****************************************************/
 
             return CreatedAtAction(
                 nameof(GetEmployee),
