890946bb2cbb46928ae99562304c3947814d1fc3
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-10 14:00:32 -0300

    Create DbContext for Employees

---
+++ KafkaTutorial/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using KafkaTutorial.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KafkaTutorial.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
