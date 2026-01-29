2ec699cf02f393fd8a92c0c7930e2333f432ad7d
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-28 23:24:01 -0300

    Store the shell scripts necessary to configure the Kafka cluster
    Use a bash terminal with confluent-cli installed. First execute start-kafka.sh, then create-topic.sh. Then run the producer and consumer C# projects.

---
+++ create-topic.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run in a bash terminal with confluent-cli installed.
+# This creates the topic purchases used in the ConfluentTutorial
+confluent local kafka topic create purchases
---
+++ start-kafka.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run in a bash terminal with confluent-cli installed.
+# This starts Kafka
+confluent local kafka start
